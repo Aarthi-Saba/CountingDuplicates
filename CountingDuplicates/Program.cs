c8f374aa574f9cfd568da138b98f1c4eb46a9a22
aarthi <aarthi.saba@gmail.com>
2020-05-06 20:08:48 -0400

    Add project files.

---
+++ CountingDuplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingDuplicates
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DuplicateCount("");
+            DuplicateCount("abcde");
+            DuplicateCount("aabbcde");
+            DuplicateCount("aabBcde");
+            DuplicateCount("Indivisibility");
+            //DuplicateCount("Indivisibilities");
+            DuplicateCount("Aa2112");
+        }
+        public static int DuplicateCount(string str)
+        {
+            int count = str.ToUpper().GroupBy(i => i).Count(l => l.Count() > 1);
+            //List<string> temp = new List<string>();
+            //string input = str.ToUpper();
+            //for(int i =0;i < input.Length;i++)
+            //{
+            //    for(int j=i+1; j < input.Length; j++)
+            //    {
+            //        if(input[i] == input[j] && !temp.Contains(input[i].ToString()))
+            //        {
+            //            temp.Add(input[i].ToString());
+            //            count += 1;
+            //            break;
+            //        }
+            //    }
+            //}
+            Console.WriteLine($"Count {count}");
+            return count;
+        }
+    }
+}
